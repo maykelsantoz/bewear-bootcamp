7a2e1ea33306b2066fe97ed2f3bcdbd01410d406
[maykelsantoz] <55636141+maykelsantoz@users.noreply.github.com>
2025-08-09 11:16:55 -0300

    chore: add eslint, prettier and shadcn setup

---
+++ src/app/product/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Product = () => {
+  return (
+    <div>
+      <Button className="my-1 bg-red-500 px-5">Bootcamp</Button>
+    </div>
+  );
+};
+
+export default Product;
