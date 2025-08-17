ed0ca8c042b04de6103bacb84212d2f74ccc399a
[maykelsantoz] <55636141+maykelsantoz@users.noreply.github.com>
2025-08-17 00:53:30 -0300

    feat: add product list and product item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,65 @@
-import { Header } from "@/components/common/header";
+import Image from "next/image";
 
-const Home = () => {
+// import CategorySelector from "@/components/common/category-selector";
+// import Footer from "@/components/common/footer";
+import { Header } from "@/components/common/header";
+import ProductList from "@/components/common/product-list";
+import { db } from "@/db";
+// import { getCategories } from "@/data/categories/get";
+// import {
+//   getNewlyCreatedProducts,
+//   getProductsWithVariants,
+// } from "@/data/products/get";
+
+const Home = async () => {
+  // const [products, newlyCreatedProducts, categories] = await Promise.all([
+  //   getProductsWithVariants(),
+  //   getNewlyCreatedProducts(),
+  //   // getCategories(),
+  // ]);
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+
   return (
     <>
       <Header />
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner-01.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductList products={products} title="Mais vendidos" />
+
+        <div className="px-5">
+          {/* <CategorySelector categories={categories} /> */}
+        </div>
+
+        <div className="px-5">
+          <Image
+            src="/banner-02.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        {/* <ProductList products={newlyCreatedProducts} title="Novos produtos" /> */}
+        {/* <Footer /> */}
+      </div>
     </>
   );
 };
+
 export default Home;
